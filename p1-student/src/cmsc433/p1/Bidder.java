07bc852f70f07672b0903412de672c20a2bfa8fa
kwang927 <57078169+kwang927@users.noreply.github.com>
2021-03-16 08:36:37 -0400

    Delete p1-student directory

--- p1-student/src/cmsc433/p1/Bidder.java
+++
@@ -1,7 +0,0 @@
-package cmsc433.p1;
-
-public interface Bidder extends Client {
-	int cash();
-	int cashSpent();
-	int mostItemsAvailable();
-}
--- p1-student/src/cmsc433/p1/Client.java
+++
@@ -1,6 +0,0 @@
-package cmsc433.p1;
-
-public interface Client extends Runnable
-{
-	String name();
-}
